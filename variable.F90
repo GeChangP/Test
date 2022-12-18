ed9c99f23e774a6c1b1f3d383f8bd774a6a51790
GeChangP <qkrdpckd@pusan.ac.kr>
2022-12-18 22:59:11 +0900

    common

---
+++ variable.F90
@@ -0,0 +1,14 @@
+Module variable
+    Real :: PI,PI05,PI2
+    Integer :: IRAO,IDIF,IRAD
+    Real(8) :: CMAS,C22,OG,KZZ,GM
+    PARAMETER (MX=105,NP=100,NQ=101)
+    Real(8) :: XP(MX),YP(MX),XQ(NQ),YQ(NQ)
+    Real(8) :: VN(3,NP)
+    Complex(8) :: ZFI(4,NP)
+    Complex(8) :: ZAB(3,3),ZEXF(3)
+    Complex(8) :: ZMTNO(3)
+
+
+
+End MODULE
